ea62574ba904e1e7c6209cdedfcc6d2021ac8e47
zhanna-naumenko <jannetj88@gmail.com>
2024-02-04 12:08:34 +0200

    Fixed the main loop. Now it breaks two loops when the password is found.

--- My_project/exam.py
+++ My_project/exam.py
@@ -11,10 +11,37 @@ secret_password = find_me + 'bcmpda'
 # ----------------------------------------
 
 letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-for first_char in letters:
-    for second_char in letters:
-        secret_password = first_char + second_char + 'bcmpda'
+password_found = False
+for char_1 in letters:
+    for char_2 in letters:
+        secret_password = char_1 + char_2 + 'bcmpda'
         if unzip_with_7z(zip_file_path, dest_path, secret_password):
+            password_found = True
             break
+    if password_found:
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#for first_char in letters:
+#    for second_char in letters:
+#        secret_password = first_char + second_char + 'bcmpda'
+#        if unzip_with_7z(zip_file_path, dest_path, secret_password):
+#            break
 
 
